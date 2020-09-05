562827cadc1edfa5bfc1c10335233a957028ae38
fg11111 <62109910+fg11111@users.noreply.github.com>
2020-09-05 15:13:27 +0800

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -9,8 +9,8 @@
  * 3. alwaysNotice控制是否重复显示已经看过的图片，如果设置为false，则只会显示更新的图片。
  */
 
-let channels = ["Orzmimi"];
-let maxMedias = 3;
+let channels = ["Orzmimi", "zh_Tumblrs"];
+let maxMedias = 10;
 let alwaysNotice = false;
 
 const $ = API("telegram");
