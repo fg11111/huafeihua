b2c1aaed5d7e842fc9e4fd58d8c51e6354262367
fg11111 <62109910+fg11111@users.noreply.github.com>
2021-02-27 18:42:18 +0800

    Update tanhua.js

--- tanhua.js
+++ tanhua.js
@@ -2,13 +2,12 @@ console.log($response.body);
 let obj=JSON.parse($response.body);
 /*obj={
   "data": {
-    "status":1
     "is_vip":1
   }
 }
 */
 
-obj.data.status=1
+obj.data.is_vip=1
 
 console.log(JSON.stringify(obj));
 $done({body: JSON.stringify(obj)})
