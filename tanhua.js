e07f8109466a0439e7dc0ba2c56821d9be2629a0
fg11111 <62109910+fg11111@users.noreply.github.com>
2021-02-27 17:46:47 +0800

    Update tanhua.js

--- tanhua.js
+++ tanhua.js
@@ -2,13 +2,8 @@ console.log($response.body);
 let obj=JSON.parse($response.body);
 /*obj={
   "data": {
-    "status": "200",
-  "msg": "返回成功",
-  "data": {
-    "member_name": 
+    "member_name": huahua,
     "mobile": "18800008888",
-    "head_pic": "https://i.loli.net/2020/07/04/NBrHSoDL1TEfdjX.png",
-    "parent_id": 888,
     "user_viptime": "2099-09-09",
     "status": 1,
     "is_vip": 1
