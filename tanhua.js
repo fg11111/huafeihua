727e6ae0b1631061b2642506d7987e4431276df7
fg11111 <62109910+fg11111@users.noreply.github.com>
2021-02-27 18:17:16 +0800

    Create tanhua.js

---
+++ tanhua.js
@@ -0,0 +1,17 @@
+console.log($response.body);
+let obj=JSON.parse($response.body);
+/*obj={
+  "data": {
+    "member_name": "hauhua",
+    "mobile": "18800008888",
+    "user_viptime": "2099-09-09",
+    "status":1
+    "is_vip":1
+  }
+}
+*/
+
+obj.data.status=1
+
+console.log(JSON.stringify(obj));
+$done({body: JSON.stringify(obj)})
